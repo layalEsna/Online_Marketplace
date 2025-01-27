91b94e6e39d79deb27397d4ad9b2e0b05eef5c6a
Amene <layla_1380@yahoo.com>
2025-01-27 13:43:06 -0600

    Create `Sellers` component in the components folder and import it into `App.js`

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Sellers from "./Sellers";
 
 function App() {
   return <h1>Project Client</h1>;
---
+++ client/src/components/Sellers.js
@@ -0,0 +1,7 @@
+
+function Sellers() {
+    
+
+
+}
+export default Sellers
