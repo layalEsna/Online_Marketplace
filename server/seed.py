59a5add41ded2701642b250412d0fd8b81733717
Amene <layla_1380@yahoo.com>
2025-01-26 20:44:23 -0600

    Add seed script to create users with predefined usernames and hashed passwords

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,35 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Product
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        User.query.delete()
+        Product.query.delete()
+
+        users_data = [
+            {'username': 'bahare', 'password': 'Bbbbbbbbb!'},
+            {'username': 'loona1', 'password': 'Lllllllll!'},
+            {'username': 'mishoul', 'password': 'Mmmmmmmmm!'},
+            {'username': 'derakht', 'password': 'Ddddddddd!'},
+            {'username': 'aseman', 'password': 'Aaaaaaaaa!'},
+        ]
+
+        users = []
+        for user_data in users_data:
+            user = User(
+                username=user_data['username'])  
+            user.password = user_data['password']
+            
+            users.append(user)
+            db.session.add(user)
+        db.session.commit()
+            
+            
+
+
