9e5a3667b22d84f120a8e36b8dbfe9a17dff4462
Amene <layla_1380@yahoo.com>
2025-01-27 13:56:54 -0600

    Create `Sellers` component to fetch and display sellers from the API

--- client/src/components/Sellers.js
+++ client/src/components/Sellers.js
@@ -1,6 +1,31 @@
-
+import React, { useState, useEffect } from "react"
 function Sellers() {
-    
+    const [sellers, setSellers] = useState([])
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5555/sellers')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Failed to get sellers.')
+                }
+                return res.json()
+            })
+            .then(data => setSellers(data))
+            .catch(e => console.error(`Internal error: ${e}`))
+    }, [])
+
+    return (
+        <div>
+            <ul>
+                {sellers.map(seller =>
+                    <li key={seller.id}>
+                        <h3>{ seller.username }</h3>
+
+                    </li>)
+                }
+            </ul>
+        </div>
+    )
 
 
 }
